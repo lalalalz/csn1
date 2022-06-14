955ec8bef9694dbf47ad93c221b27e26f64da879
lalalalz <lalalalz@naver.com>
2022-06-14 20:17:09 +0900

    #ADD
    1. cjs 원격 저장소에 커밋함

--- src/main/java/csn/opggclone/OpggcloneApplication.java
+++ src/main/java/csn/opggclone/OpggcloneApplication.java
@@ -8,6 +8,6 @@ public class OpggcloneApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpggcloneApplication.class, args);
+		System.out.println("this is a test code");
 	}
-
 }
