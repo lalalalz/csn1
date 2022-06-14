ff73d849f5b7c44daaf389134d6c44eab7b14e86
lalalalz <lalalalz@naver.com>
2022-06-14 20:45:47 +0900

    원격 저장소 병합 테스트2 (shc)
    원격 저장소 병합 테스트2 (shc)

--- src/main/java/csn/opggclone/OpggcloneApplication.java
+++ src/main/java/csn/opggclone/OpggcloneApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OpggcloneApplication {
 
 	public static void main(String[] args) {
-	SpringApplication.run(OpggcloneApplication.class, args);  
+		SpringApplication.run(OpggcloneApplication.class, args);  
 	}
 
 }
