db9fbaff2889ad5bb58bf4624ddaa44b5e6ef421
lalalalz <lalalalz@naver.com>
2022-06-14 17:20:01 +0900

    #INIT 최초 커밋

---
+++ src/main/java/csn/opggclone/OpggcloneApplication.java
@@ -0,0 +1,13 @@
+package csn.opggclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpggcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpggcloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/csn/opggclone/OpggcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package csn.opggclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpggcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
